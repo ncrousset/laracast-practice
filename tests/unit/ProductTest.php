c4f6f99277132a5e40e8912f530b92bd8e0cdd50
Rudys Natanael Acosta Crousset <natanael926@gmail.com>
2016-03-21 17:23:42 -0400

    Beginning Unit Testing final

--- tests/unit/ProductTest.php
+++ tests/unit/ProductTest.php
@@ -8,19 +8,23 @@ use App\Product;
 */
 class ProductTest extends PHPUnit_Framework_TestCase
 {
+
+	protected $product;
+
+	public function setUp()
+	{
+		$this->product = new Product('Fallout 4', 59);
+	}
 	
+	/** @test */
 	function testAProductHasName()
 	{
-		$product = new Product('Fallout 4', 59);
-
-		$this->assertEquals('Fallout 4', $product->name());
-
+		$this->assertEquals('Fallout 4', $this->product->name());
 	}
 
+	/** @test */
 	function testAProductHasACost()
 	{
-		$product = new Product('Fallout 4', 59);
-
-		$this->assertEquals(59, $product->cost());
+		$this->assertEquals(59, $this->product->cost());
 	}
 }
